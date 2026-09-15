7845d1ee7c9c15b5125f4aa135e6dd7c6b0d4b40
ptrachoo <67011511@kmitl.ac.th>
2026-09-15 20:55:44 +0700

    ex00 but jQuery

--- cell03/ex04/ex00bis/background.js
+++ cell03/ex04/ex00bis/background.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     function getRandomColor() {
-        const hex = '0123456789ABCDEF';
+        const letters = '0123456789ABCDEF';
         let color = '#';
         for (let i = 0; i < 6; i++) {
-            color += hex[Math.floor(Math.random() * 16)];
+            color += letters[Math.floor(Math.random() * 16)];
         }
         return color;
     }
